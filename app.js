5ce1f1e68fa8b6b0a588f9e939f7ac4c7ccbd39c
vampirot800 <111137463+vampirot800@users.noreply.github.com>
2024-06-12 14:21:35 -0600

    Lab 6 + 11

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+// Middleware para manejar datos de formularios
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Importar rutas
+const modulo1Rutas = require('./rutasm1');
+const modulo2Rutas = require('./rutasm2');
+
+// Usar rutas
+app.use('/rutasm1', modulo1Rutas);
+app.use('/rutasm2', modulo2Rutas);
+
+// Ruta 5: Ruta adicional en el archivo principal
+app.get('/ruta5', (request, response) => {
+    response.send('Respuesta de la ruta "/ruta5"');
+});
+
+// Manejo de rutas no existentes
+app.use((request, response) => {
+    response.status(404).send('Página no encontrada');
+});
+
+app.listen(3001, () => {
+    console.log('Servidor corriendo en el puerto 3001');
+});
